cee884cee5c56ce25c8f05e86f9e325c8d7a92e6
Michael Niers <michael.niers@ewetel.net>
2019-08-30 12:28:04 +0200

    Code Upload

---
+++ commands.sh
@@ -0,0 +1,12 @@
+function create() {
+    cd
+    python create.py $1
+    cd /Users/User/Documents/Projects/MyProjects/$1
+    git init
+    git remote add origin git@github.com:Username/$1.git
+    touch README.md
+    git add .
+    git commit -m "Initial commit"
+    git push -u origin master
+    code .
+}
